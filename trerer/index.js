6918eb05af1061a7955eeda490079b39e66d417a
PRO-IRON <134826496+PRO-IRON@users.noreply.github.com>
2025-05-13 15:38:12 +0530

    1sr

---
+++ trerer/index.js
@@ -0,0 +1,34 @@
+let homeSc = document.getElementById("homesc")
+let gueSc = document.getElementById("guesc")
+
+let homeSco = 0
+let gueSco = 0
+
+function addh1() {
+    homeSco += 1
+    homeSc.innerText = homeSco
+}
+
+function addh2() {
+    homeSco += 2
+    homeSc.innerText = homeSco
+}
+function addh3() {
+    homeSco += 3
+    homeSc.innerText = homeSco
+}
+//-----------------------------------
+function add1() {
+    gueSco += 1
+    gueSc.innerText = gueSco
+}
+
+function add2() {
+    gueSco += 2
+    gueSc.innerText = gueSco
+}
+function add3() {
+    gueSco += 3
+    gueSc.innerText = gueSco
+}
+
